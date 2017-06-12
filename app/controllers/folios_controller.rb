7ad9abeed30780cfe6b4c9b958b078c612a1ceec
Scott Young <young.scottw@gmail.com>
2017-06-12 17:58:11 -0500

    implement folio new and create actions

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -3,4 +3,20 @@ class FoliosController < ApplicationController
     @folio_items = Folio.all
   end
   
+  def new
+    @folio_item = Folio.new
+  end
+  
+  def create
+    @folio_item = Folio.new(params.require(:folio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @folio_item.save
+        format.html { redirect_to folios_path, notice: 'Your portfolio item is now live.' }
+      else
+        formate.html { render :new }
+      end
+    end
+  end
+  
 end
