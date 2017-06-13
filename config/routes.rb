179aa32a3ea9cb0fbb53ce238ef8674f112d74ee
Scott Young <young.scottw@gmail.com>
2017-06-13 12:28:50 -0500

    implement enum for blog status

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
 
   resources :folios, except: [:show]
   get 'folio/:id', to: 'folios#show', as: 'folio_show'
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  
 end
