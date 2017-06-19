e4e39431b7cbed7b66133a77f1d3b1930bd3e8f5
Scott Young <young.scottw@gmail.com>
2017-06-19 10:40:51 -0500

    implement auth rules for folio drag and drop feature

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -1,7 +1,7 @@
 class FoliosController < ApplicationController
   before_action :set_folio, only: [:edit, :show, :update, :destroy]
   layout 'folio'
-  access all: [:show, :index, :react], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :react], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @folio_items = Folio.by_position
