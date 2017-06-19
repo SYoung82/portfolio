8f947e4b4e654c10bfd6de4a3e7b09cb8682cb03
Scott Young <young.scottw@gmail.com>
2017-06-19 14:47:33 -0500

    implemented initial file uploading systsystem via carrierwave for folio items

--- app/models/folio.rb
+++ app/models/folio.rb
@@ -6,6 +6,9 @@ class Folio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, FolioUploader
+  mount_uploader :main_image, FolioUploader
+
   def self.by_position
     order("position ASC")
   end
