e932db7ee65aabe1dc6ecd5e23a56b5df938af69
Scott Young <young.scottw@gmail.com>
2017-06-22 14:30:48 -0500

    update production.rb for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://syoung-portfolio.herokuapp.com', 'http://syoung-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://syoung-portfolio.herokuapp.com/cable"
 end
